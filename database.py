2317d5721d2f0e7cb87a7ddc23529e264a7018ed
OmegaWolf <omega.wolf0401@gmail.com>
2025-04-24 00:44:10 +0200

    Ajout de commentaire

--- database.py
+++ database.py
@@ -4,19 +4,19 @@ from tabulate import tabulate
 db_path = "D:\Code\Projet_en_cours\Task Manager\data\data.db"
 
 class Data:
-    def __init__(self,path):
+    def __init__(self,path): #Init de la connexion avec la base de donnée
         self.path = path
         self.data = sql.connect(self.path)
 
-    def query(self,query):
+    def query(self,query): #Retourne le resultat d'une requete SQL
         cur = self.data.cursor()
         return list(cur.execute(query))
     
-    def dev_show_table(self,table):
-        tab = self.query(f"SELECT * FROM {table}")
-        headers_raw = self.query(f"PRAGMA table_info({table})")
+    def dev_show_table(self,table): #Renvoie le contenue d'une table SQL dans la console de manière lisible
+        tab = self.query(f"SELECT * FROM {table}") #Récupère tout le contenue de la table
+        headers_raw = self.query(f"PRAGMA table_info({table})") #Récupère le nom et propriété de toute les colonne de la table
         header = list()
-        for element in headers_raw:
+        for element in headers_raw: #Récupère uniquement le nom des colonnes des propriété des colonne de la table
             header.append(element[1])
         print(tabulate(tab,header,tablefmt="outline"))
 
